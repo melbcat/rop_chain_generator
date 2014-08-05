6df7666bef68f45ecf931f471e6f1c3807ec3b29
vc <vince18546@hotmail.com>
2014-08-05 18:46:26 +0800

    Add Linked list for gadgets chaining
    * Use arg to read binary file
    * Use linked list for gadgets chaining

--- main.c
+++ main.c
@@ -1,29 +1,32 @@
 #include "rop.h"
 
-int main(void)
+int main(int argc, char** argv)
 {
-        FILE *fp;
-        char file_name[20];
-	char *binary;
-        unsigned long binary_len;
+    FILE *fp;
+    unsigned char *binary;
+    unsigned long binary_len;
+    int result = 0;	
 
-        printf("Enter binary file name: ");
-        scanf("%s",file_name);
-        fp = fopen(file_name, "rb");
+    if(argc < 2){
+        printf("./ropchain <Binary File>\n");
+        return -1;
+    }
+    fp = fopen(argv[1], "rb");
 
-        //Get file length
-        fseek(fp, 0, SEEK_END);
-        binary_len=ftell(fp);
-        fseek(fp, 0, SEEK_SET);
+    //Get file length
+    fseek(fp, 0, SEEK_END);
+    binary_len=ftell(fp);
+    fseek(fp, 0, SEEK_SET);
 
-        //Allocate memory
-        binary = (char *)malloc(binary_len+1);
-        if(fp){
-                fread(binary,binary_len,1,fp);
-        }   
-        fclose(fp);
-
-	rop_findgadgets(binary, binary_len);
-	free(binary);
-	return 0;
+    //Allocate memory
+    binary = (unsigned char *)malloc(binary_len+1);
+    if(fp){
+        result = fread(binary,binary_len,1,fp);
+    }   
+    if(result > 0){
+        rop_findgadgets(binary, binary_len);
+    }
+    free(binary);
+    fclose(fp);
+    return 0;
 }
--- rop.h
+++ rop.h
@@ -9,11 +9,23 @@
 #include <capstone/capstone.h>
 
 struct Gadget{
-        char string[500];
-        unsigned int address;
+    char string[500];
+    unsigned int address;
+    struct Gadget *prev;
+    struct Gadget *next; 
 };
 
-int rop_findgadgets(char *binary, unsigned long binary_len);
-int rop_find(char* operate, char* operand, size_t count, cs_insn *insn, struct Gadget GADGET);
+struct Chain_List{
+    int count;
+    struct Gadget *first;
+    struct Gadget *last;
+};
+
+int rop_findgadgets(unsigned char *binary, unsigned long binary_len);
+int rop_find(char* operate, char* operand, size_t count, cs_insn *insn, struct Chain_List *LIST);
+
+void rop_chain_list_init(struct Chain_List *LIST);
+void rop_chain_list_add(struct Chain_List *LIST, unsigned int address, char *string);
+void rop_chain_list_traverse(struct Chain_List *LIST);
 
 #endif
